54349bf2bf08c7d9bdc0a8ec110980906e88a02d
monikazeng <monikaqz12@gmail.com>
2023-08-01 18:36:42 +0100

    React ql5

--- ReactPractice/superheroes-ui/src/App.js
+++ ReactPractice/superheroes-ui/src/App.js
@@ -27,15 +27,37 @@ function App() {
       </Navbar>
 
       <Routes>
-        <Route path="/allheroes" element={<AllHeroes/>} />
-        <Route path="/addhero" element={<HeroForm/>} />
-        <Route path="/" element={<AllHeroes/>} />
+        <Route path="/allheroes" element={<AllHeroes />} />
+        <Route path="/addhero" element={<HeroForm />} />
+        <Route path="/" element={<AllHeroes />} />
       </Routes>
 
     </>
-
-  
   );
 }
+// return(
+// <AllHeroes/>
+// <HeroDetail/>
+
+// <div className="App">
+//   <header className="App-header">
+//     <img src={logo} className="App-logo" alt="logo" />
+//     <p>
+//       Edit <code>src/App.js</code> and save to reload.
+//     </p>
+//     <a
+//       className="App-link"
+//       href="https://reactjs.org"
+//       target="_blank"
+//       rel="noopener noreferrer"
+//     >
+//       Learn React
+//     </a>
+//   </header>
+// </div>
+// )
+
+
+
 
 export default App;
--- ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
+++ ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import { React, useEffect, useState } from 'react'
+// import React from 'react'
 import images from "../index"
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 // import batman from '../images/batman.jpeg';
 
 const HeroDetail = (props) => {
+  const[counter, setCounter] = useState(0);
+  const incrementCounter =() => {
+    setCounter(counter+1);
+  }
   return (
     <Card >
       <div className="imgContainer">
@@ -13,7 +18,8 @@ const HeroDetail = (props) => {
       <Card.Body >
         <Card.Title >Alias: {props.info.alias}</Card.Title>
         <Card.Text >Name: {props.info.name}</Card.Text>
-        <Button variant="primary">Like</Button>
+        <Button variant="primary" onClick={incrementCounter}>Like</Button>
+        <Card.Text>Likes: {counter}</Card.Text>
       </Card.Body>
     </Card>
   )
